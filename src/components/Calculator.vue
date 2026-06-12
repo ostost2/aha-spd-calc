bf999647552ffb2f03261f1528143c962ef3a89c
ostost2 <ritzip.rit@gmail.com>
2026-06-12 16:58:29 +0800

    colors

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,18 +1,20 @@
 <script setup lang="ts">
-import { computed, ref } from 'vue'
+import { computed, reactive, ref } from 'vue'
 
-const list = ref([100]);
+const randomHsl = () => `hsla(${Math.random() * 360}, 80%, 50%, 1)`
 
-const sorted = computed(() => [...list.value].sort((a, b) => b - a))
+const list = reactive([{spd: 100, color: randomHsl()}]);
+
+const sorted = computed(() => [...list].sort((a, b) => b.spd - a.spd))
 
 const speed = computed(() => {
   let spd = 80;
   let scale = 5;
   for (let i of sorted.value) {
-    spd += i / scale;
+    spd += i.spd / scale;
     scale *= 2;
   }
-  return isNaN(spd) ? "?" : spd;
+  return isNaN(spd) ? "?" : spd.toFixed(1);
 })
 </script>
 
@@ -23,16 +25,16 @@ const speed = computed(() => {
     <div id="inputs">
       <p id="label">Elation teammate's<br>speed values:</p>
       <div>
-        <div v-for="(spd, index) in list">
-          <input class="field" type="number" :value="spd"
-            @input="e => list[index] = Number.parseFloat((e.target as HTMLInputElement)?.value)">
-          <input class="delete" v-if="index > 0" type="button" value="×" @click="list.splice(index, index)">
+        <div v-for="(entry, index) in list">
+          <input class="field" type="number" :value="entry.spd" :style="{color: entry.color}"
+            @input="e => list[index].spd = Number.parseFloat((e.target as HTMLInputElement)?.value)">
+          <input class="delete" v-if="index > 0" type="button" value="×" @click="list.splice(index, 1)">
         </div>
-        <div v-if="list.length < 4"><input class="field" type="button" value="+" @click="_ => list.push(100)"></div>
+        <div v-if="list.length < 4"><input class="field" type="button" value="+" @click="_ => list.push({spd: 100, color: randomHsl()})"></div>
       </div>
     </div>
 
     <p>Add your roster's speed values in any order</p>
-    <p>80<template v-for="(spd, index) in sorted"> + {{ spd }} / {{ 5 * 2 ** index }}</template> = <b>{{ speed }}</b></p>
+    <p>80<template v-for="(entry, index) in sorted"> + <span :style="{color: entry.color}">{{ entry.spd }}</span> / {{ 5 * 2 ** index }}</template> = <b>{{ speed }}</b></p>
   </section>
 </template>
