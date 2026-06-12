7be3b62d9a6bfbb8979cacfbce43005872dd9b74
ostost2 <ritzip.rit@gmail.com>
2026-06-12 17:14:52 +0800

    suspiciously strict linter

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { computed, reactive, ref } from 'vue'
+import { computed, reactive } from 'vue'
 
 const randomHsl = () => `hsla(${Math.random() * 360}, 80%, 50%, 1)`
 
